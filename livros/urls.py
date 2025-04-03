3fa01a851b4a567f189f1e40c030d592633d35eb
GabrielBoscoDeolindo <gabibosco01@gmail.com>
2025-04-03 15:04:04 -0300

    correcao url

--- livros/urls.py
+++ livros/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('livros/', views.listar_livros),
-    path('criarlivro/', views.criar_livro),
+    path('/api/livros/', views.listar_livros),
+    path('/api/criarlivro/', views.criar_livro),
 ]
