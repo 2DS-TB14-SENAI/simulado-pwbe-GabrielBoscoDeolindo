9fe587cc0ff124c8014a61b961dcff5c1bc9b20f
GabrielBoscoDeolindo <gabibosco01@gmail.com>
2025-04-03 15:28:04 -0300

    e outra

--- livros/urls.py
+++ livros/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('livros/', views.listar_livros),
-    path('criarlivro/', views.criar_livro),
+    path('livros/', views.criar_livro),
     path('api/livros/', views.listar_livros),
-    path('api/criarlivro/', views.criar_livro),
+    path('api/livros/', views.criar_livro),
 ]
