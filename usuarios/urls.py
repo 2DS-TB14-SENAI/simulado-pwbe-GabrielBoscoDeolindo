53b84f2c2b450582800c64bad35cf69fc4f78e7e
GabrielBoscoDeolindo <gabibosco01@gmail.com>
2025-04-03 16:38:33 -0300

    mais uma '/'

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('auth/registro', view=views.create_user, name="create_user"),
+    path('auth/registro/', view=views.create_user, name="create_user"),
     path('auth/login/', view=views.login, name='login'),
 ]
