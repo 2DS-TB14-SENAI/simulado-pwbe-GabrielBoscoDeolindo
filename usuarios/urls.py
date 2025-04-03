134df06cc5a55dae9ccf216fb4be060b78fef9fa
GabrielBoscoDeolindo <gabibosco01@gmail.com>
2025-04-03 16:37:12 -0300

    adicao de uma '/' nas urls

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('auth/registro', view=views.create_user, name="create_user"),
-    path('auth/login', view=views.login, name='login'),
+    path('auth/login/', view=views.login, name='login'),
 ]
