1358fed173d1074bbc279cdb93cbfbe753d8e513
GabrielBoscoDeolindo <gabibosco01@gmail.com>
2025-04-03 15:53:04 -0300

    correcao das views e urls

--- livros/urls.py
+++ livros/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('livros/', views.listar_livros),
-    path('criarlivro/', views.criar_livro),
-    path('api/livros/', views.listar_livros),
-    path('api/criarlivro/', views.criar_livro),
+    path('livros/', views.livros),
+    path('criarlivro/', views.livros),
+    path('api/livros/', views.livros),
+    path('api/livros/', views.livros),
 ]
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('criarUsuario/', view=views.create_user, name="create_user"),
+    path('criarusuario/', view=views.create_user, name="create_user"),
     path('logar/', view=views.login, name='login'),
 ]
